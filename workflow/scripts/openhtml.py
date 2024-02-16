dd4d048f2dfd4adff452d4abe3e368da5c4b41a6
Teresia Mrema-Buza <ndelly@gmail.com>
2024-02-16 17:06:25 +0300

    Update openhtml.py

--- workflow/scripts/openhtml.py
+++ workflow/scripts/openhtml.py
@@ -12,18 +12,3 @@ if system in commands:
 else:
     print(f"Unsupported operating system ({system}). Please open the 'html' file manually.")
 
-# # Alternative code
-# import subprocess
-# import platform
-
-# # Open html files using Bash command
-# if platform.system() == "Darwin":  # macOS
-#     subprocess.run(["open", "images/mymap.html"])
-# elif platform.system() == "Windows":  # Windows
-#     subprocess.run(["start", "images/mymap.html"])
-# elif platform.system() == "Linux":  # Linux
-#     subprocess.run(["xdg-open", "images/mymap.html"])
-# else:  # Other systems
-#     print("Unsupported operating system. Please open the 'html' file manually.")
-
-
